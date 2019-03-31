b77cc66ef1afeaaac96f0521879b9a4f8c9bbe51
Lucy Blatherwick <blatherwick.lucy@gmail.com>
2019-03-31 17:58:24 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,65 +18,64 @@ public class Main {
         Helpers hp = new Helpers();
 
         // Parse JSON and put data into db
-//        JSONObject jo = null;
-//        try {
-//
-//            jo = (JSONObject) new JSONParser().parse(new FileReader("positions.json"));
-//
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        JSONArray teams = (JSONArray) jo.get("teams");
-//        int count = 0;
-//        for(Object x : teams){
-//            JSONObject team = (JSONObject) x;
-//            Long serial = (Long) team.get("serial");
-//            Long marker = (Long) team.get("marker");
-//            String name = (String) team.get("name");
-//
-//            //Enter into db
-//            db.insertTeam(serial, marker, name);
-//            //System.out.println("***********"  +serial+ "************");
-//
-//            JSONArray positionList = (JSONArray) team.get("positions");
-//            for(Object y : positionList){
-//                JSONObject position = (JSONObject) y;
-//
-//                Boolean alert = (Boolean) position.get("alert");
-//                Long altitude  = (Long) position.get("altitude");
-//                String type = (String) position.get("type");
-//                Double dtfKm = (Double) position.get("dtfKm");
-//                Long pos_id = (Long) position.get("pos_id");
-//                Long gpsAt = hp.convertToTimestamp((String) position.get("gpsAt"));
-//                Double sogKnots = (Double) position.get("sogKnots");
-//                Long battery = (Long) position.get("battery");
-//                Long cog = (Long) position.get("cog");
-//                Double dtfNm = (Double) position.get("dtfNm");
-//                Long txAt = hp.convertToTimestamp((String) position.get("txAt"));
-//                Double longitude = (Double) position.get("longitude");
-//                Double latitude = (Double) position.get("latitude");
-//                Long gpsAtMillis = (Long) position.get("gpsAtMillis");
-//                Double sogKmph = (Double) position.get("sogKmph");
-//
-//                //insert into db
-//                db.insertPosition(name, alert, altitude, type, dtfKm, pos_id, gpsAt,sogKnots, battery, cog, dtfNm, txAt, longitude, latitude, gpsAtMillis, sogKmph);
-//
-//                if(count % 100 == 0){
-//                    System.out.println("Inserted item: "+count);
-//                }
-//                count++;
-//            }
-//
-//            //Iterate through positionList
-//            //for each iteration, add entry into db
-//
-//        }
+        JSONObject jo = null;
+        try {
 
+            jo = (JSONObject) new JSONParser().parse(new FileReader("positions.json"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray teams = (JSONArray) jo.get("teams");
+        int count = 0;
+        for(Object x : teams){
+            JSONObject team = (JSONObject) x;
+            Long serial = (Long) team.get("serial");
+            Long marker = (Long) team.get("marker");
+            String name = (String) team.get("name");
+
+            //Enter into db
+            db.insertTeam(serial, marker, name);
+            //System.out.println("***********"  +serial+ "************");
+
+            JSONArray positionList = (JSONArray) team.get("positions");
+            for(Object y : positionList){
+                JSONObject position = (JSONObject) y;
+
+                Boolean alert = (Boolean) position.get("alert");
+                Long altitude  = (Long) position.get("altitude");
+                String type = (String) position.get("type");
+                Double dtfKm = (Double) position.get("dtfKm");
+                Long pos_id = (Long) position.get("pos_id");
+                Long gpsAt = hp.convertToTimestamp((String) position.get("gpsAt"));
+                Double sogKnots = (Double) position.get("sogKnots");
+                Long battery = (Long) position.get("battery");
+                Long cog = (Long) position.get("cog");
+                Double dtfNm = (Double) position.get("dtfNm");
+                Long txAt = hp.convertToTimestamp((String) position.get("txAt"));
+                Double longitude = (Double) position.get("longitude");
+                Double latitude = (Double) position.get("latitude");
+                Long gpsAtMillis = (Long) position.get("gpsAtMillis");
+                Double sogKmph = (Double) position.get("sogKmph");
+
+                //insert into db
+                db.insertPosition(name, alert, altitude, type, dtfKm, pos_id, gpsAt,sogKnots, battery, cog, dtfNm, txAt, longitude, latitude, gpsAtMillis, sogKmph);
+
+                if(count % 100 == 0){
+                    System.out.println("Inserted item: "+count);
+                }
+                count++;
+            }
+
+            //Iterate through positionList
+            //for each iteration, add entry into db
+
+        }
 
         // Decide on method to determine number of other vessels visible
         // -> boats will be visible if they are less than 10km away (roughly)
